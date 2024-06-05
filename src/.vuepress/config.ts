bde53066b63c6df61f312e2829b49711f7ccc274
dwiky <dwiky@jayantara.net>
2024-06-05 16:05:52 +0700

    Perubahan Font Family dan Color theory

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,6 +4,12 @@ import theme from "./theme.js";
 export default defineUserConfig({
   base: "/",
 
+  head: [
+    ["link",{rel: "preconnect", href: "https://fonts.googleapis.com"}],
+    ["link",{rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: ""}],
+    ["link",{rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"}]
+  ],
+
   locales: {
     "/": {
       lang: "en-US",
